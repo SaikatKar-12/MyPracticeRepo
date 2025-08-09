cd9c0286aa8a56af78629e53f58d316a02379d61
Saikat Kar <ksaikat009@gmail.com>
2025-08-09 22:34:41 +0530

    week 7 work

---
+++ week5_microservices/6416675/Microservices/account/src/main/java/com/cognizant/account/AccountController.java
@@ -0,0 +1,42 @@
+package com.cognizant.account;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts")
+public class AccountController {
+
+    @GetMapping("/{number}")
+    public Account getAccountDetails(@PathVariable String number) {
+        return new Account(number, "savings", 234343);
+    }
+
+    // could have also created a separate model class for Account
+    // but for simplicity I am using an inner class here
+    static class Account {
+        private String number;
+        private String type;
+        private double balance;
+
+        public Account(String number, String type, double balance) {
+            this.number = number;
+            this.type = type;
+            this.balance = balance;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public double getBalance() {
+            return balance;
+        }
+    }
+}
---
+++ week6_react/React-Hol/Code/myfirstreact/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>Welcome to the first Session of React</h1>
+  );
+}
+
+export default App;
+
+
